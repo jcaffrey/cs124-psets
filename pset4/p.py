c8c1ce4ad0f79b38de33e0c04e33d38c780ebbcd
HSADev2 <hsadev2@HSADev2s-iMac.local>
2017-03-04 11:40:50 -0500

    do it

--- pset4/p.py
+++ pset4/p.py
@@ -1,9 +1,12 @@
 word_ct = 0
 p_store = {}  # key: word_ct/index of word, value: penalty of that word, ? - also store line_num?
-M = 20
+M = 40
 
 
 words = []
+line_br = {} # key: line_num, value: word at end of line
+line_ct = 0
+br = []
 
 for line in reversed(open('buffy.txt').readlines()):
     for word in reversed(line.strip().split()):
@@ -11,33 +14,39 @@ for line in reversed(open('buffy.txt').readlines()):
         if word_ct == 0:
             p_store[word_ct] = 0
             word_ct += 1
-        # update the penalty cost for that word (act as if 'pulling up' all other possible words)
-            #need to know num of words that can fit on next line -->
-            #gives spaces, error for that line-->take the min of all cases
+        # update the penalty cost for that word (act as if 'pulling up' all other possible words) need to know num of words that can fit on next line -->gives spaces, error for that line-->take the min of all cases
         tmp = []
         tmp_words = []
         tmp_word_length = 0
         space_ct = 0
-        for k in reversed(range(0, word_ct)):
+        for k in reversed(range(1, word_ct + 1)):
             space_ct += 1
-            tmp_word_length += len(words[k])
-            if M - len(word) - tmp_word_length - space_ct >= 0 and word_ct - k - 1 in p_store.keys():  # if word fits on same line
-                # print 'got here with word_ct = ' + str(word_ct) + ' and k = ' + str(k)
-                tmp.append(M - len(word) - tmp_word_length - space_ct + p_store[word_ct - k - 1]) # don't want len(word) here?
+            tmp_word_length += len(words[k - 1])
+            if M - len(word) - tmp_word_length - space_ct >= 0 and word_ct - k  in p_store.keys():  # if word fits on same line
+                # print 'word_ct = ' + str(word_ct) + ' k-1 = ' + str(k-1) + ' tmp_word_length ' + str(tmp_word_length) + ' wordlen ' + str(len(words[k - 1]))
+                tmp.append(M - len(word) - tmp_word_length - space_ct + p_store[word_ct - k ]) # don't want len(word) here?
         try:
-            print tmp
-            p_store[word_ct] = min(tmp)
+            # print tmp
+            #FIGURE OUT HERE WHAT MIN TELLS US ABOUT # OF WORDS TO PULL UP...
+            t = min(tmp)
+            tmp_new = []
+            for ele in tmp:
+                if M - ele >= 0:
+                    tmp_new.append(M-ele)
+            p_store[word_ct] = min(tmp_new)
+            print t, tmp_new
         except:
             pass
-        # print min(tmp)
-        # print extra(word_ct, k) for k in range(0, word_ct)
-        # p_store[word_ct] = min(extra(word_ct,k) + p_store[word_ct - k] for k in range(0, word_ct))
-        # p_store[word_ct] = min(M - len_words - num_words + p_store[word_ct - ])
+
         print word_ct
         word_ct += 1
+
+    print 'LINE NUM IS ' + str(line_ct)
+    line_ct += 1
 print p_store
 tot = 0
 for k in p_store.keys():
+    # only add costs of each line...then cube that val
     tot += p_store[k]
 print tot
 # print words
