eec202884385d344fc1dcf02ca3f9824d44603b1
vagrant <jcaffrey@college.harvard.edu>
2017-03-18 22:48:43 +0000

    added struct but seg faulting

--- pa2/strassen.c
+++ pa2/strassen.c
@@ -11,27 +11,27 @@
 
 #define CUT 32
 
-// me(m, sz, i, j)
-//    Return a pointer to matrix element `m[i][j]` -- the element
-//    at row `i` and column `j`. The matrix is square with dimension
-//    `sz`. Requires: `i < sz && j < sz`
-static inline int* me(int* m, size_t sz, size_t i, size_t j) {
-    return &m[i * sz + j];
-}
+typedef struct {
+    int r_start;
+    int r_end;
+    int c_start;
+    int c_end;
+    int* elements;
+} matrix;
 
-// fast_matrix_multiply(c, sz, a, b)
-//    `a`, `b`, and `c` are square matrices with dimension `sz`.
-//    Computes the matrix product `a x b` and stores it in `c`.
-void conventional_multiply(int* c, size_t sz, int* a, int* b) 
+#define ELEMENT(m, r, c) m->elements[(c-1) * m->r_end + (r-1)]
+
+/*matrix conventional_multiply(matrix c, int sz, matrix a, matrix b)
 {
     // clear `c`
-    for (size_t i = 0; i < sz; ++i)
-        for (size_t j = 0; j < sz; ++j)
+    for (int i = 0; i < sz; ++i)
+        for (int j = 0; j < sz; ++j)
+            c.elements[i][j]
             *me(c, sz, i, j) = 0;
 
     // compute product and update `c`
         // want to read left to right for both
-    for (size_t i = 0; i < sz; ++i)     
+    for (size_t i = 0; i < sz; ++i)
         for (size_t k = 0; k < sz; ++k)
             for (size_t j = 0; j < sz; ++j)
                 *me(c, sz, i, j) += *me(a, sz, i, k) * *me(b, sz, k, j);
@@ -48,7 +48,7 @@ void strassen(int* c, size_t n, int* a, int*b)
         return;
     }
 }
-
+*/
 int main(int argc, char* argv[]) 
 {
     int d;
@@ -65,10 +65,21 @@ int main(int argc, char* argv[])
         d = atoi(argv[2]);
 
         // allocate matrices
+        matrix* a = (matrix*) malloc(sizeof(matrix));
+        a->r_start = 0;
+        a->r_end = d;
+        a->c_start = 0;
+        a->c_start = d;
+        a->elements = (int*) malloc(sizeof(int) * d * d);
+
+/*        matrix b;
+        b.elements = malloc(sizeof(int) * d * d);
+*/
+        /*
         int* a = (int*) malloc(sizeof(int) * (size_t)d * (size_t)d);
         int* b = (int*) malloc(sizeof(int) * (size_t)d * (size_t)d);
         int* c = (int*) malloc(sizeof(int) * (size_t)d * (size_t)d);
-
+*/
         if(in == NULL) {
             printf("Error reading file\n");
             return 1;            
@@ -88,14 +99,14 @@ int main(int argc, char* argv[])
             if(tot < d * d) 
             {
                 // add to matrix A
-                *me(a, d, i, j) = val;
+                ELEMENT(a, i, j) = val;
             }
-            if(tot >= d*d && tot < 2 * d *d)
+/*            if(tot >= d*d && tot < 2 * d *d)
             {
                 // add to matrix B
-                *me(b, d, i, j) = val;
+                b.elements[i][j] = val;
 
-            }
+            }*/
 
             tot++;
             j++;
@@ -108,27 +119,31 @@ int main(int argc, char* argv[])
                 i = 0;
         }
         // print input matrices
-/*
-        // print matrices
-        for(i = 0; i < d; i++)
-            for(j = 0; j < d; j++)
-                printf("%d\n", *me(a, (size_t)d, (size_t)i, (size_t)j));
 
+        // print matrices
+        int tmp =0;
         for(i = 0; i < d; i++)
             for(j = 0; j < d; j++)
-                printf("%d\n", *me(b, (size_t)d, (size_t)i, (size_t)j));
+            {
+                tmp = ELEMENT(a, i, j);
+                printf("%d\n", tmp);
+            }
+                
+/*        for(i = 0; i < d; i++)
+            for(j = 0; j < d; j++)
+                printf("%d\n", b.elements[i][j]);
 */
         // compute `c = a x b`
-        strassen(c, (size_t)d, a, b);  // this is how to call..
+//        strassen(c, (size_t)d, a, b);  // this is how to call..
 
-        // print result matrix
+/*        // print result matrix
         for(i = 0; i < d; i++)
             for(j = 0; j < d; j++)
                 printf("%d\n", *me(c, (size_t)d, (size_t)i, (size_t)j));
         
-        free(a);
-        free(b);
-        free(c);
+*/        free(a);
+        //free(b);
+       // free(c);
 
     }
 
