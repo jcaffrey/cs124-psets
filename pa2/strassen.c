d8c0ddbf8d3e1c8cfa6e4694b7b53b962860f1a9
vagrant <jcaffrey@college.harvard.edu>
2017-03-19 03:26:55 +0000

    got conventional working with structs

--- pa2/strassen.c
+++ pa2/strassen.c
@@ -19,70 +19,92 @@ typedef struct {
     int* elements;
 } matrix;
 
-#define ELEMENT(m, r, c) m->elements[(c-1) * m->r_end + (r-1)]
+/*static inline int* me(int* ele, int sz, int i, int j) {
+    return &ele[i * sz + j];
+}*/
+//#define ELEMENT(m, r, c) m->elements[(c) * m->r_end + (r)]
 
-/*matrix conventional_multiply(matrix c, int sz, matrix a, matrix b)
+#define ELEMENT(m, i, j) m->elements[(m->r_end - m->r_start) * i + j]
+
+void conventional_multiply(matrix* c, int sz, matrix* a, matrix* b)
 {
     // clear `c`
     for (int i = 0; i < sz; ++i)
         for (int j = 0; j < sz; ++j)
-            c.elements[i][j]
-            *me(c, sz, i, j) = 0;
+            ELEMENT(c, i, j) = 0;
 
     // compute product and update `c`
         // want to read left to right for both
-    for (size_t i = 0; i < sz; ++i)
-        for (size_t k = 0; k < sz; ++k)
-            for (size_t j = 0; j < sz; ++j)
-                *me(c, sz, i, j) += *me(a, sz, i, k) * *me(b, sz, k, j);
+    for (int i = 0; i < sz; ++i)
+        for (int k = 0; k < sz; ++k)
+            for (int j = 0; j < sz; ++j)
+                ELEMENT(c, i, j) += ELEMENT(a, i, k) * ELEMENT(b, k, j);
 }
 
-void strassen(int* c, size_t n, int* a, int*b)
+void strassen(matrix* c, int n, matrix* a, matrix*b)
 {
     if(n <= CUT)
         conventional_multiply(c, n, a, b);
     else
     {
         // break a, b, c up using index calculations
-        int a11 = a
+        //int a11 = a
         return;
     }
 }
-*/
-int main(int argc, char* argv[]) 
+
+
+matrix * newMatrix(int rs, int re, int cs, int ce) {
+    int d;
+    d = 0;
+    if((re - rs) != (ce - cs)) {
+        printf("Warning: instatiate square matrices only!\n");
+        return NULL;
+    }
+    else
+    {
+        d = re - rs;
+    }
+
+    matrix * nm = (matrix *) malloc(sizeof(matrix));
+
+    // define indices
+    nm->r_start = rs;
+    nm->r_end = re;
+    nm->c_start = cs;
+    nm->c_end = ce;
+
+    // allocate a double array of length rows * cols
+    nm->elements = (int *) malloc(d * d * sizeof(int));
+    // set all data to 0
+    int i;
+    for (i = 0; i < d * d; i++)
+        nm->elements[i] = 0.0;
+
+    return nm;
+}
+
+int main(int argc, char* argv[])
 {
     int d;
-    // size_t sz = 1000;
-    FILE *in;
-
+    FILE* in; 
     if(argc != 4) {
         printf("Proper usage: ./strassen [optional] [dimension] [inputfile]\n");
         return 1;
     }
-    else 
+    else
     {
         in = fopen(argv[3], "r");
         d = atoi(argv[2]);
 
-        // allocate matrices
-        matrix* a = (matrix*) malloc(sizeof(matrix));
-        a->r_start = 0;
-        a->r_end = d;
-        a->c_start = 0;
-        a->c_start = d;
-        a->elements = (int*) malloc(sizeof(int) * d * d);
+        matrix* a = newMatrix(0, d, 0, d);
+        matrix* b = newMatrix(0, d, 0, d);
+        matrix* c = newMatrix(0, d, 0, d);
 
-/*        matrix b;
-        b.elements = malloc(sizeof(int) * d * d);
-*/
-        /*
-        int* a = (int*) malloc(sizeof(int) * (size_t)d * (size_t)d);
-        int* b = (int*) malloc(sizeof(int) * (size_t)d * (size_t)d);
-        int* c = (int*) malloc(sizeof(int) * (size_t)d * (size_t)d);
-*/
-        if(in == NULL) {
+        if(in == NULL)
+        {
             printf("Error reading file\n");
-            return 1;            
+            return 1; 
         }
 
         int i = 0;
@@ -90,24 +112,14 @@ int main(int argc, char* argv[])
         int tot = 0;
         int val;
 
-        while(fscanf(in, "%d", &val) != EOF) 
+        while(fscanf(in, "%d", &val) != EOF)
         {
-            if(i == j)
-            {
+            if(i==j)
                 printf("%d\n", val);
-            }
-            if(tot < d * d) 
-            {
-                // add to matrix A
+            if(tot < d * d)
                 ELEMENT(a, i, j) = val;
-            }
-/*            if(tot >= d*d && tot < 2 * d *d)
-            {
-                // add to matrix B
-                b.elements[i][j] = val;
-
-            }*/
-
+            if(tot >= d * d && tot < 2 * d *d)
+                ELEMENT(b, i, j) = val;
             tot++;
             j++;
             if(j == d)
@@ -118,35 +130,14 @@ int main(int argc, char* argv[])
             if(tot == d * d)
                 i = 0;
         }
-        // print input matrices
-
-        // print matrices
-        int tmp =0;
+        strassen(c, d, a, b);
         for(i = 0; i < d; i++)
+        {
             for(j = 0; j < d; j++)
             {
-                tmp = ELEMENT(a, i, j);
-                printf("%d\n", tmp);
+                printf("%d\n", ELEMENT(c, i, j));
             }
-                
-/*        for(i = 0; i < d; i++)
-            for(j = 0; j < d; j++)
-                printf("%d\n", b.elements[i][j]);
-*/
-        // compute `c = a x b`
-//        strassen(c, (size_t)d, a, b);  // this is how to call..
-
-/*        // print result matrix
-        for(i = 0; i < d; i++)
-            for(j = 0; j < d; j++)
-                printf("%d\n", *me(c, (size_t)d, (size_t)i, (size_t)j));
-        
-*/        free(a);
-        //free(b);
-       // free(c);
-
+        }
     }
-
-
 
 }
